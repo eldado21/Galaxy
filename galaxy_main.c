44156435d4075cb80ec3533a3ebd56cdb15b70f0
Damian Boquete <damian.boquete@hotmail.com>
2025-04-10 11:29:35 +0200

    Added arg support

--- galaxy_main.c
+++ galaxy_main.c
@@ -1,7 +1,19 @@
 #include "galaxy.h"
 
-int main(){
-    int size = 4000;
+int main(int argc, char* argv[]){
+    int size = 10000;
+
+    printf("%d\n", argc);
+    if (argc == 2) {
+        // status stores user input value or 0 if atoi error
+        int status = atoi(argv[1]);
+
+        if (status != 0 && status < 50000) {
+            size = status;
+            printf("Using user input: %d entities\n", size);
+        }
+    }
+    
     vec screenBounds;
     int xyBound = 600;
     molecule bh;
